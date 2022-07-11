c4ab52137544378fee418865e41e33438a4fefcc
han-see <74469036+han-see@users.noreply.github.com>
2022-07-11 20:42:16 +0200

    Initial commit

---
+++ hardhat.config.ts
@@ -0,0 +1,58 @@
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-etherscan";
+import "@nomiclabs/hardhat-waffle";
+import "@typechain/hardhat";
+import "dotenv/config";
+import "hardhat-deploy";
+import "hardhat-gas-reporter";
+import { HardhatUserConfig } from "hardhat/types";
+import "solidity-coverage";
+
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "https://eth-rinkeby";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "0x";
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "0x";
+
+const config: HardhatUserConfig = {
+    solidity: {
+        compilers: [{ version: "0.8.7" }, { version: "0.8.0" }],
+    },
+    defaultNetwork: "hardhat",
+    networks: {
+        hardhat: {
+            chainId: 31337,
+        },
+        rinkeby: {
+            url: RINKEBY_RPC_URL,
+            chainId: 4,
+            accounts: [PRIVATE_KEY],
+        },
+        localhost: {
+            chainId: 31337,
+        },
+    },
+    etherscan: {
+        apiKey: ETHERSCAN_API_KEY,
+    },
+    gasReporter: {
+        enabled: false,
+        outputFile: "gas-reporter.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
+        token: "ETH",
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+        player: {
+            default: 1,
+        },
+    },
+    mocha: {
+        timeout: 3000000, // 300 seconds
+    },
+};
+
+export default config;
