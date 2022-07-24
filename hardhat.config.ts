ee905f7625f1136b0392c82b7e7b986ae0211651
han-see <74469036+han-see@users.noreply.github.com>
2022-07-24 20:52:18 +0200

    Add forking url

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,26 +1,30 @@
-import "@nomiclabs/hardhat-ethers";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
-import "dotenv/config";
-import "hardhat-deploy";
-import "hardhat-gas-reporter";
-import { HardhatUserConfig } from "hardhat/types";
-import "solidity-coverage";
+import "@nomiclabs/hardhat-ethers"
+import "@nomiclabs/hardhat-etherscan"
+import "@nomiclabs/hardhat-waffle"
+import "@typechain/hardhat"
+import "dotenv/config"
+import "hardhat-deploy"
+import "hardhat-gas-reporter"
+import { HardhatUserConfig } from "hardhat/types"
+import "solidity-coverage"
 
-const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "https://eth-rinkeby";
-const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x";
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "0x";
-const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "0x";
+const RINKEBY_RPC_URL = process.env.RINKEBY_RPC_URL || "https://eth-rinkeby"
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x"
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "0x"
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY || "0x"
+const MAINNET_RPC_URL = process.env.MAINNET_RPC_URL || ""
 
 const config: HardhatUserConfig = {
     solidity: {
-        compilers: [{ version: "0.8.7" }],
+        compilers: [{ version: "0.8.7" }, { version: "0.6.6" }],
     },
     defaultNetwork: "hardhat",
     networks: {
         hardhat: {
             chainId: 31337,
+            forking: {
+                url: MAINNET_RPC_URL,
+            },
         },
         rinkeby: {
             url: RINKEBY_RPC_URL,
@@ -53,6 +57,6 @@ const config: HardhatUserConfig = {
     mocha: {
         timeout: 3000000, // 300 seconds
     },
-};
+}
 
-export default config;
+export default config
