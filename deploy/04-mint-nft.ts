f7b13afda90b9da3a92c78d22cdf12203c40868d
han-see <74469036+han-see@users.noreply.github.com>
2022-07-26 21:26:54 +0200

    Add mint script for random nft

--- deploy/04-mint-nft.ts
+++ deploy/04-mint-nft.ts
@@ -1,11 +1,31 @@
 import { BigNumber } from "ethers"
-import { ethers, getNamedAccounts } from "hardhat"
+import { ethers, getNamedAccounts, network } from "hardhat"
 import { DeployFunction } from "hardhat-deploy/dist/types"
 import { HardhatRuntimeEnvironment } from "hardhat/types"
-import { DynamicSvgNft } from "../typechain-types"
+import { DynamicSvgNft, RandomIpfsNft } from "../typechain-types"
 
 const mintNft: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
     const { deployer } = await getNamedAccounts()
+
+    //Random IpfsNft
+    const randomIpfsNft: RandomIpfsNft = await ethers.getContract("RandomIpfsNft", deployer)
+    const mintFee = await randomIpfsNft.getMintFee()
+    const randomIpfsNftTx = await randomIpfsNft.requestNft({ value: mintFee.toString() })
+    const randomIpfsNftTxReceipt = await randomIpfsNftTx.wait(1)
+
+    await new Promise(async (resolve) => {
+        setTimeout(resolve, 300000)
+        randomIpfsNft.once("NftMinted", () => {
+            resolve("")
+        })
+        if (network.config.chainId == 31337) {
+            const requestId = randomIpfsNftTxReceipt.events![1].args!.requestId.toString()
+            const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
+            await vrfCoordinatorV2Mock.fulfillRandomWords(requestId, randomIpfsNft.address)
+        }
+    })
+
+    console.log(`Random Ipfs NFT minted, ${randomIpfsNftTxReceipt.transactionHash}`)
 
     //Dynamic Nft mint
     const dynamicNft: DynamicSvgNft = await ethers.getContract("DynamicSvgNft", deployer)
@@ -15,4 +35,4 @@ const mintNft: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
 }
 
 export default mintNft
-mintNft.tags = ["mint"]
+mintNft.tags = ["all", "mint"]
